33df923319c772935ede6f97ff05eb6c864e98c7
Vincent Spiewak <vspiewak@gmail.com>
2010-03-14 21:19:39 +0000

    ooo 3.2 patch (102164)

--- src/com/versusoft/packages/ooo/odt2daisy/addon/gui/UnoGUI.java
+++ src/com/versusoft/packages/ooo/odt2daisy/addon/gui/UnoGUI.java
@@ -100,7 +100,13 @@ public class UnoGUI {
     private boolean isFullExport = false;
 
     public UnoGUI(XComponentContext m_xContext, XFrame m_xFrame){
+
         this(m_xContext, m_xFrame, false);
+
+        // Bug 102164 OOo 3.2 (DEV300m48)
+        // http://www.openoffice.org/issues/show_bug.cgi?id=102164
+        Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
+
     }
 
     public UnoGUI(XComponentContext m_xContext, XFrame m_xFrame, boolean isFullExport) {
